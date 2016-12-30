48dde530b4d30683f238cc6627855032d3bda6f8
Haven Giguere <haven.giguere@gmail.com>
2016-12-30 14:50:52 -0800

    adding more mobingo code

---
+++ moBingo/js/script.js
@@ -0,0 +1,31 @@
+
+function LoadBingoArray() {
+	var binArray = Array.from(Array(76).keys());
+	var binNums = binArray.slice(1);
+	var shuffNums = _.shuffle(binNums);
+	return shuffNums;
+}
+var shuffNums = LoadBingoArray();
+
+function PropBinNum() {
+	var disNum = shuffNums.shift();
+	if (disNum <= 15) {
+		disNum = "B" + "<br>" + disNum;
+		document.getElementById("white-square").innerHTML = disNum;
+	} else if (disNum > 15 && disNum <= 30) {
+		disNum = "I" + "<br>" + disNum;
+		document.getElementById("white-square").innerHTML = disNum;
+	} else if (disNum > 30 && disNum <= 45) {
+		disNum = "N" + "<br>" + disNum;
+		document.getElementById("white-square").innerHTML = disNum;
+	} else if (disNum > 45) {
+		disNum = "N" + "<br>" + disNum;
+		document.getElementById("white-square").innerHTML = disNum;
+	} else {
+		disNum = "The" + "<br>" + "End";
+		document.getElementById("white-square").innerHTML = disNum;
+	}
+	
+}
+
+// setTimeout(function(){ alert("Hello"); }, 4000);
