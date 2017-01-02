82a49c6fcf16987b8cf45736cf119b9259f9450c
Haven Giguere <haven.giguere@gmail.com>
2017-01-01 20:59:06 -0800

    Bingo app with bingo card start

--- moBingo/js/script.js
+++ moBingo/js/script.js
@@ -18,17 +18,39 @@ function PropBinNum() {
 	} else if (disNum > 30 && disNum <= 45) {
 		disNum = "N" + "<br>" + disNum;
 		document.getElementById("white-square").innerHTML = disNum;
-	} else if (disNum > 45) {
-		disNum = "N" + "<br>" + disNum;
+	} else if (disNum > 45 && disNum <=60) {
+		disNum = "G" + "<br>" + disNum;
+		document.getElementById("white-square").innerHTML = disNum;
+	} else if (disNum > 60 && disNum <=75) {
+		disNum = "O" + "<br>" + disNum;
 		document.getElementById("white-square").innerHTML = disNum;
 	} else {
 		disNum = "The" + "<br>" + "End";
 		document.getElementById("white-square").innerHTML = disNum;
-		document.getElementById("newGame").innerHTML = "New Game?";
+	}
+	BingoCard(disNum);
+}
+
+// function Refresh() {
+// 	window.location="index.html";
+// }
+
+
+
+function BingoCard(disNum){
+	var cardNum = disNum.slice(5);
+	console.log(cardNum);
+	if (cardNum <= 15) {
+    	document.getElementById("bNum").innerHTML = cardNum;
+  	}else if (cardNum > 15 && cardNum <= 30) {
+		document.getElementById("iNum").innerHTML = cardNum;
+	} else if (cardNum > 30 && cardNum <= 45) {
+		document.getElementById("nNum").innerHTML = cardNum;
+	} else if (cardNum > 45 && cardNum <=60) {
+		document.getElementById("gNum").innerHTML = cardNum;
+	} else if (cardNum > 60 && cardNum <=75) {
+		document.getElementById("oNum").innerHTML = cardNum;
 	}
 }
 
-function Refresh() {
-	window.location="index.html";
-}
 // setTimeout(function(){ alert("Hello"); }, 4000);
